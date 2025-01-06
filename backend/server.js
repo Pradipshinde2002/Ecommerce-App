50a919edf6af8887be89573ea3162f4c7d441cfa
Pradip Shinde <74617409+Pradipshinde2002@users.noreply.github.com>
2025-01-06 20:41:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,30 +24,10 @@ connectCloudinary()
 
 //middlewares
 app.use(express.json())
-// app.use(cors())   //added corsOptions 
-
-
+app.use(cors())   //added corsOptions 
 
 
 //added 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // If cookies or auth tokens are required
-  })
-);
-
-
-
-
 
 //api endpoints
 app.use('/api/user',userRouter)

ecf885e24c87bc0aff777cf5e40ee6eb8c689726
Pradip Shinde <74617409+Pradipshinde2002@users.noreply.github.com>
2025-01-07 14:12:34 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,35 +88,23 @@ const registerUser = async (req, res) => {
 
 // route for admin login
 
+const adminLogin = async (req, res) => {
+  try{
 
-const adminLogin = (req, res) => {
-    const { email, password } = req.body;
+    const {email,password}=req.body
 
-    if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-        const token = jwt.sign({ email, password }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        return res.status(200).json({ success: true, token });
-    } else {
-        return res.status(401).json({ success: false, message: "Invalid email or password." });
+    if(email===process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+      const token=jwt.sign(email+password,process.env.JWT_SECRET)
+      res.json({success:true,token})
     }
+    else{
+      res.json({success:false,message:'invalid credentials'})
+    }
+  }
+  catch(error){
+    console.log(error)
+    res.json({success:false,message:error.message})
+  }
 };
-
-// const adminLogin = async (req, res) => {
-//   try{
-
-//     const {email,password}=req.body
-
-//     if(email===process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
-//       const token=jwt.sign(email+password,process.env.JWT_SECRET)
-//       res.json({success:true,token})
-//     }
-//     else{
-//       res.json({success:false,message:'invelid credentials'})
-//     }
-//   }
-//   catch(error){
-//     console.log(error)
-//     res.json({success:false,message:error.message})
-//   }
-// };
 
 export { loginUser, registerUser, adminLogin };

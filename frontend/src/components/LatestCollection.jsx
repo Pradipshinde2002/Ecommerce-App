525bfb97c09cfd13502cbb74f92b97cc532648ec
Pradip Shinde <74617409+Pradipshinde2002@users.noreply.github.com>
2025-01-08 15:54:34 +0530

    Update LatestCollection.jsx

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -16,7 +16,7 @@ const LatestCollection = () => {
   return (
     <div className='my-10'>
       <div className='text-center py-8 text-3xl'>
-        <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+        <Title text1={'LATEST '} text2={'COLLECTIONS'} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, quae necessitatibus. Provident esse explicabo omnis dolores in at odit nihil cum porro numquam deleniti odio, fugit similique. Qui, quo consectetur?
         </p>
